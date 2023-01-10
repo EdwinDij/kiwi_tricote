142d2788088c011998a315a6970d8b674e2ff529
EdwinDij <edwin.d899@gmail.com>
2023-01-10 12:43:38 +0100

    les image s'affichent faire le css de la mise en page

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from kiwi_tricote import views
+from django.conf.urls.static import static
+
+from .settings import MEDIA_URL, MEDIA_ROOT
+
 urlpatterns = [
+    path('', views.index),
     path('adminjdpabmqnft/', admin.site.urls),
-    path('pat/', views.index, name='index'),
-]
+       
+] + static(MEDIA_URL, document_root = MEDIA_ROOT )
--- blog/kiwi_tricote/models.py
+++ blog/kiwi_tricote/models.py
@@ -6,5 +6,9 @@ from django.db import models
 # picture
 
 class Articles(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, default='')
     text = models.TextField()
+    image = models.ImageField(upload_to='media/')
+    
+    def __str__(self) -> str:
+        return self.title
